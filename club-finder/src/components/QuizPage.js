1775c7d8145bfb5e8411ad06ec986093ac91ab58
unknown <Cal30470@gmail.com>
2024-12-01 13:44:22 -0800

    Commited from desktop

--- club-finder/src/components/QuizPage.js
+++ club-finder/src/components/QuizPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../style/QuizPage.css';
+import { Link } from "react-router-dom";
 
 const QuizPage = () => {
     return (
         <div className="quiz-page">
             <h1>Find Your Perfect Club</h1>
             <p>Answer a few questions to discover clubs that match your interests.</p>
-            <button className="start-quiz-btn">Start Quiz</button>
+            <Link to="/question" className="question-link">
+                Start Quiz
+            </Link>
         </div>
     );
 };
