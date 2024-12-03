8820960f2233d01517f9430d1125828e2a79ae17
Eyob Menjigso <eyobgirma1993@gmail.com>
2024-12-02 20:54:33 -0800

    Finalized the featured clubs on the homepage and added the contact info section

--- club-finder/src/components/FeaturedClubs.js
+++ club-finder/src/components/FeaturedClubs.js
@@ -1,23 +1,26 @@
 // src/components/FeaturedClubs.js
 import React from 'react';
 import '../style/FeaturedClubs.css'; // Styling for the component
+import communityImage from '../assets/community.png';
+import young from '../assets/young.jpg';
+import mathClubImage from '../assets/math_club.jpg';
 
 const FeaturedClubs = () => {
   const clubs = [
     {
       name: 'Outdoor Rec',
       description: 'Outdoor Rec organizes weekend trips, rents gear, and supports adventures for the PLU community.',
-      imgSrc: '/assets/community.png', 
+      imgSrc: communityImage, 
     },
     {
       name: 'PLU Young Life',
       description: 'Young Life creates community, fun, and explores the Christian faith.',
-      imgSrc: '/assets/eco-club.jpg', 
+      imgSrc: young, 
     },
     {
       name: 'Math Club',
       description: 'PLU Math Club is a space to explore and play with a wide variety of mathematical ideas!',
-      imgSrc: '/assets/math_club.jpg',
+      imgSrc: mathClubImage,
     },
   ];
 
